e6060ea49b308f7614449f7791e2f800870b99e3
viniciusdes <viniciussilva9200@gmail.com>
2024-05-26 09:53:04 -0300

    feat: json-server only

--- index.js
+++ index.js
@@ -15,35 +15,9 @@ const app = express();
 app.use(express.json());
 
 app.get("/usuarios", async (req, res) => {
-  // console.log("req", req.query);
-  // if (req.query) {
-  //   return res.json(
-  //     await axios
-  //       .get("http://localhost:3001/usuarios/", {
-  //         params: req.query,
-  //       })
-  //       .then((res) => {
-  //         return (res = res.data);
-  //       })
-  //   );
-  // }
-  const consulta = await axios
-    .get("http://localhost:3001/usuarios/")
-    .then((res) => {
-      return (res = res.data);
-    });
+  const consulta = (await axios.get("http://localhost:3001/usuarios/")).data;
 
   return res.json(consulta);
 });
 
-// app.post("/usuarios", async (req, res) => {
-//   return await axios
-//     .post("http://localhost:3001/usuarios/", req.body)
-//     .then((res) => {
-//       return (res = res.data);
-//     });
-// });
-
 server.listen(3001, () => console.log("server"));
-
-// app.listen(8800, () => console.log("app"));
